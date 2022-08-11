05a3884a74cea02e1e02afdb644603698c894c3a
zxy <247176265@qq.com>
2022-08-11 23:25:36 +0800

    feat: add light ,camera and sky

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -32,22 +32,22 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right"):
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = SPEED
-		$MeshInstance.rotate_z(deg2rad(-SPEED_ROLL))
-	elif Input.is_action_pressed("ui_right"):
 		velocity.x = -SPEED
 		$MeshInstance.rotate_z(deg2rad(SPEED_ROLL))
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+		$MeshInstance.rotate_z(deg2rad(-SPEED_ROLL))
 	else:
 		velocity.x = lerp(velocity.x, 0, 0.1)
 	
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		velocity.z = 0
 	elif Input.is_action_pressed("ui_up"):
-		velocity.z = SPEED
-		$MeshInstance.rotate_x(deg2rad(SPEED_ROLL))
-	elif Input.is_action_pressed("ui_down"):
 		velocity.z = -SPEED
 		$MeshInstance.rotate_x(deg2rad(-SPEED_ROLL))
+	elif Input.is_action_pressed("ui_down"):
+		velocity.z = SPEED
+		$MeshInstance.rotate_x(deg2rad(SPEED_ROLL))
 	else:
 		velocity.z = lerp(velocity.z, 0, 0.1)
 		
